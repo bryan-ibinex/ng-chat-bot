d8dfa8f1d6ff80672e63c955974702215d636b3b
Bryan Ribo <ribobryan@gmail.com>
2018-05-03 18:27:35 +1000

    Initial Commit Chat Bot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 
+import { ChatModule } from './chat/chat.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: 'fd8c22962b8446c084b14351d1d3fd31'
+  }
 };
